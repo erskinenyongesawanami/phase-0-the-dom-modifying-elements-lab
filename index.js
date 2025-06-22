c7e450b7c081a1a67886159921301974c7294111
erskinenyongesawanami <erskineviper@gmail.com>
2025-06-23 00:33:51 +0300

    add changes made

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the existing <main> with id 'main'
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set its id to 'victory'
+newHeader.id = 'victory';
+
+// Set its text content to your name + " is the champion"
+newHeader.textContent = 'Moha Afif is the champion';
+
+// Add the new element to the document body
+document.body.append(newHeader);
